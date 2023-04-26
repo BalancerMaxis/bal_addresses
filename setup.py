002257c2dd424a3104e276f877b1290684c81018
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-04-27 00:19:27 +0200

    a bit more cleanup

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    python_requires="==3.9",
     url="https://github.com/BalancerMaxis/bal-maxi-addresses",
     install_requires=["pandas", "pathlib"],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
