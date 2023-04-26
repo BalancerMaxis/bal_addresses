e2f6b4b85ae4f1ba8c2dee4f5c59eb9b71279516
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-04-27 00:28:48 +0200

    fix src, almost there.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 LONG_DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 
