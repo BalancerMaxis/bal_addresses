b03a0a60872532039b5cc90198cb381ca15ea861
tritium-vlk <11885129+Tritium-VLK@users.noreply.github.com>
2023-04-27 00:14:50 +0200

    restructure

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .addresses import ( read_addressbook, write_addressbooks, addressbook_by_chain, read_reversebook, address_lookup_dict, checksum_address_dict)
-
-__all__ = ["read_addressbook", "write_addressbooks", "addressbook_by_chain", "read_reversebook", "address_lookup_dict", "checksum_address_dict"]
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 LONG_DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 
 # Setting up
 setup(
-    # the name must match the folder name 'verysimplemodule'
-    name="bal-maxi-addresses",
+    name="bal_addresses",
     version=VERSION,
     author="Tritium",
     author_email="<nope@email.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    python_requires="==3.9",
+    url="https://github.com/BalancerMaxis/bal-maxi-addresses",
     install_requires=["pandas", "pathlib"],  # add any additional packages that
     # needs to be installed along with your package. Eg: 'caer'
 
     keywords=['python', 'first package'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: DeFi",
         "Programming Language :: Python :: 3.9",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
