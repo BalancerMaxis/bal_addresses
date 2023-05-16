c5bb906746e306a794db3a2680a0f91ff7d81872
Tritium <tritium-vlk@telegram.works>
2023-05-16 15:26:36 +0200

    bump version to 0.4.0 for integration testing with other usecases.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.5'
+VERSION = '0.4.0'
 DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 LONG_DESCRIPTION = 'Balancer Maxi Monorepo Addressbook'
 
