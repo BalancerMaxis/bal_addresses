bab58df0c38ed799d648280eb5583eae4ed2cdaf
Tritium <11885129+Tritium-VLK@users.noreply.github.com>
2023-05-22 23:06:44 +0200

    Use new deployments repo (#30)
    * use new blabs deployments repo.
    
    * add missing deployments env vars lost in merge.
    
    ---------
    
    Co-authored-by: Tritium <tritium-vlk@telegram.works>

--- bal_addresses/addresses.py
+++ bal_addresses/addresses.py
@@ -11,16 +11,18 @@ SCANNERS_BY_CHAIN = chains["SCANNERS_BY_CHAIN"]
 
 
 
+
 ### Main class
 class AddrBook:
     chains = DotMap(requests.get(
         f"https://raw.githubusercontent.com/BalancerMaxis/bal_addresses/main/extras/chains.json").json())
     GITHUB_MONOREPO_RAW = "https://raw.githubusercontent.com/balancer-labs/balancer-v2-monorepo/master"
     GITHUB_MONOREPO_NICE = "https://github.com/balancer/balancer-v2-monorepo/blob/master"
+    GITHUB_DEPLOYMENTS_RAW = "https://raw.githubusercontent.com/balancer/balancer-deployments/master"
+    GITHUB_DEPLOYMENTS_NICE = "https://github.com/balancer/balancer-deployments/blob/master"
     GITHUB_RAW_OUTPUTS = "https://raw.githubusercontent.com/BalancerMaxis/bal_addresses/main/outputs"
     ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
-    CHAIN_IDS_BY_NAME = chains["CHAIN_IDS_BY_NAME"]
-    SCANNERS_BY_CHAIN = chains["SCANNERS_BY_CHAIN"]
+
     fullbook = requests.get(f"{GITHUB_RAW_OUTPUTS}/addressbook.json").json()
     fx_description_by_name = requests.get("https://raw.githubusercontent.com/BalancerMaxis/bal_addresses/main/extras/func_desc_by_name.json").json
 
