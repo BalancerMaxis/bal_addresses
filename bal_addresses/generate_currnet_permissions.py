272481a8d7f5ae3f015f01b7dddcac57354b11e4
Tritium <tritium-vlk@telegram.works>
2023-07-03 01:14:01 +0200

    simplify active permissions data/restructure a bit.

--- bal_addresses/generate_currnet_permissions.py
+++ bal_addresses/generate_currnet_permissions.py
@@ -2,22 +2,24 @@ import requests
 import json
 import pandas as pd
 import os
-from addresses import AddrBook
+from bal_addresses import AddrBook
 from web3 import Web3
 import datetime
 
 INFURA_KEY = os.getenv('WEB3_INFURA_PROJECT_ID')
 
 w3_by_chain = {
+    "gnosis": Web3(Web3.HTTPProvider(f"https://gnosischain-rpc.gateway.pokt.network")),
+    "zkevm": Web3(Web3.HTTPProvider(f"https://zkevm-rpc.com")),
+    "avalanche": Web3(Web3.HTTPProvider(f"https://api.avax.network/ext/bc/C/rpc")),
+    ### Less reliable RPCs first to fail fast :)
     "mainnet": Web3(Web3.HTTPProvider(f"https://mainnet.infura.io/v3/{INFURA_KEY}")),
     "arbitrum": Web3(Web3.HTTPProvider(f"https://arbitrum-mainnet.infura.io/v3/{INFURA_KEY}")),
     "optimism": Web3(Web3.HTTPProvider(f"https://optimism-mainnet.infura.io/v3/{INFURA_KEY}")),
     "polygon": Web3(Web3.HTTPProvider(f"https://polygon-mainnet.infura.io/v3/{INFURA_KEY}")),
-    "gnosis": Web3(Web3.HTTPProvider(f"https://rpc.gnosischain.com/")),
     "goerli": Web3(Web3.HTTPProvider(f"https://goerli.infura.io/v3/{INFURA_KEY}")),
     "sepolia": Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/{INFURA_KEY}")),
-    "zkevm": Web3(Web3.HTTPProvider(f"https://zkevm-rpc.com")),
-    "avalanche": Web3(Web3.HTTPProvider(f"https://api.avax.network/ext/bc/C/rpc")),
+
 }
 
 def build_chain_permissions_list(chain_name):
@@ -37,28 +39,16 @@ def build_chain_permissions_list(chain_name):
         print(f"Processing {deployment}")
         for contract, data in contracts.items():
             for fx, action_id in data["actionIds"].items():
+                if action_id in results:
+                    continue # already have data
                 numMembers = authorizer.functions.getRoleMemberCount(action_id).call()
                 if numMembers > 0:
                     memberAddressList = []
-                    memberNameList = []
                     for i in range(0, numMembers, 1):
                         caller = (str(authorizer.functions.getRoleMember(action_id, i).call()))
                         memberAddressList.append(caller)
-                        if isinstance(address_names[caller], str):
-                            memberNameList.append(address_names[caller])
-                        else:
-                            memberNameList.append("undef")
 
-                    d = {
-                        "Fx": fx,
-                        "Contract": contract,
-                        "Deployment": deployment,
-                        "Authorized_Caller_Addresses": memberAddressList,
-                        "Authorized_Caller_Names": memberNameList,
-                    }
-                    assert action_id not in results.items(), \
-                        f"actionId {action_id} already found {results[action_id][deployment]}/{results[action_id][contract]}/{results[action_id][fx]}"
-                    results[action_id] = d
+                    results[action_id] = memberAddressList
     return results
 
 
