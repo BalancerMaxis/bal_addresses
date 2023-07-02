8738595c12fae7be30410dafb000cfdf31fc2a3e
Tritium <tritium-vlk@telegram.works>
2023-07-03 01:52:07 +0200

    finish refactor.

--- bal_addresses/permissions.py
+++ bal_addresses/permissions.py
@@ -34,31 +34,41 @@ class BalPermissions:
         self.chain = chain
         self.ACTIVE_PERMISSIONS_BY_ACTION_ID = requests.get(f"{self.GITHUB_RAW_OUTPUTS}/permissions/active/{chain}.json").json()
         self.ACTION_IDS_BY_CONTRACT_BY_DEPLOYMENT = requests.get(f"{self.GITHUB_DEPLOYMENTS_RAW}/action-ids/{chain}/action-ids.json").json()
+
+        # Define
         self.fx_paths_by_action_id = defaultdict(list)
+        self.action_id_by_fx = {}
         self.action_id_by_fx_path = {}
+        # Populate
         for deployment, contracts in self.ACTION_IDS_BY_CONTRACT_BY_DEPLOYMENT.items():
             for contract, contract_data in contracts.items():
                 for fx, action_id in contract_data["actionIds"].items():
                     fx_path = f"{deployment}/{contract}/{fx}"
                     self.fx_paths_by_action_id[action_id].append(fx_path)
-                    assert fx_path not in self.role_by_fx_path.values(), f"{fx_path} shows up twice?"
-                    self.role_by_fx_path[fx_path] = action_id
+                    assert fx_path not in self.action_id_by_fx_path.values(), f"{fx_path} shows up twice?"
+                    self.action_id_by_fx_path[fx_path] = action_id
+                    self.action_id_by_fx[fx] = action_id
 
     def search_fx(self, substr):
-        search = [s for s in self.role_by_fx_path.keys() if substr in s]
-        results = {key: self.role_by_fx_path[key] for key in search if key in self.role_by_fx_path}
+        search = [s for s in self.action_id_by_fx.keys() if substr in s]
+        results = {key: self.action_id_by_fx[key] for key in search if key in self.action_id_by_fx}
+        return results
+
+    def search_fx_path(self, substr):
+        search = [s for s in self.action_id_by_fx_path.keys() if substr in s]
+        results = {key: self.action_id_by_fx_path[key] for key in search if key in self.action_id_by_fx_path}
         return results
 
     def search_many_fxs_by_unique_deployment(self, deployment_substr, fx_substr):
         a = AddrBook(self.chain)
         results = []
         deployment = a.search_unique_deployment(deployment_substr)
-        deployment_fxs = self.search_fx(deployment).keys()
+        deployment_fxs = self.search_fx_path(deployment).keys()
         search = [s for s in deployment_fxs if fx_substr in s]
         for r in search:
             result = DotMap({
                 "fx_path": r,
-                "action_id": self.role_by_fx_path[r]
+                "action_id": self.action_id_by_fx_path[r]
             })
             results.append(result)
         return results
@@ -89,12 +99,5 @@ class BalPermissions:
         names = [a.flatbook.get(item, 'undef') for item in addresslist]
         return names
 
-    def selector_name_by_action_id(self, action_id):
-        try:
-            path = self.fx_paths_by_action_id[action_id][0]
-        except:
-            raise self.NoResultError(f"{action_id} not found")
-        (deployment, contract, fx_selector) = path.split("/")
-        return fx_selector
 
 
