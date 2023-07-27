856ad01884e419f211da8e9d74e3605c431a11ba
Tritium <tritium-vlk@telegram.works>
2023-07-27 03:19:01 +0200

    fix init

--- bal_addresses/__init__.py
+++ bal_addresses/__init__.py
@@ -1,3 +1,3 @@
-from .addresses import AddrBook, GITHUB_DEPLOYMENTS_RAW, GITHUB_DEPLOYMENTS_NICE, GITHUB_RAW_OUTPUTS, GITHUB_RAW_EXTRA
+from .addresses import AddrBook, GITHUB_DEPLOYMENTS_RAW, GITHUB_DEPLOYMENTS_NICE, GITHUB_RAW_OUTPUTS, GITHUB_RAW_EXTRAS
 from .permissions import BalPermissions
 from .errors import Errors
