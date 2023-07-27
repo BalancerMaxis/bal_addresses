31c15b9a7987e29b544a8ed5fdbd92130cf781ff
Tritium <tritium-vlk@telegram.works>
2023-07-27 02:11:15 +0200

    add based rpc

--- bal_addresses/generate_currnet_permissions.py
+++ bal_addresses/generate_currnet_permissions.py
@@ -9,6 +9,7 @@ import datetime
 INFURA_KEY = os.getenv('WEB3_INFURA_PROJECT_ID')
 
 w3_by_chain = {
+    "base": Web3(Web3.HTTPProvider("https://developer-access-mainnet.base.org")),
     "gnosis": Web3(Web3.HTTPProvider(f"https://gnosischain-rpc.gateway.pokt.network")),
     "zkevm": Web3(Web3.HTTPProvider(f"https://zkevm-rpc.com")),
     "avalanche": Web3(Web3.HTTPProvider(f"https://api.avax.network/ext/bc/C/rpc")),
