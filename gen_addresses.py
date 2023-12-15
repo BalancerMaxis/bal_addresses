ea8aa8d0e015887179ff3a14fd94d0eb47f19db2
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-12-15 15:48:37 +0100

    fix: sort reverse dict by keys

--- gen_addresses.py
+++ gen_addresses.py
@@ -1,12 +1,13 @@
-import os
-from bal_addresses import AddrBook
-import requests
 import json
+
+from bal_addresses import AddrBook
+
 
 def reverse_dict(d):
     d = dict(d)
-    inv_map = {v: k for k, v in d.items()}
+    inv_map = dict(sorted({v: k for k, v in d.items()}.items()))
     return inv_map
+
 
 def write_addressbooks(chainlist=AddrBook.chain_ids_by_name.keys()):
     for chain in chainlist:
@@ -18,10 +19,12 @@ def write_addressbooks(chainlist=AddrBook.chain_ids_by_name.keys()):
         with open(f"outputs/{chain}_reverse.json", "w") as f:
             json.dump(reverse_dict(flatbook), f, indent=3)
 
+
 def main():
     chains = AddrBook.chain_ids_by_name.keys()
     print(f"Generating new addressbook jsons for {chains}")
     write_addressbooks(chains)
+
 
 if __name__ == "__main__":
     main()
