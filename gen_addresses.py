f623c6312b3e61b453a2338884ff39b750ad0a08
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-12-19 15:32:33 +0100

    feat: also sort non reverse addr book by key

--- gen_addresses.py
+++ gen_addresses.py
@@ -14,7 +14,7 @@ def write_addressbooks(chainlist=AddrBook.chain_ids_by_name.keys()):
         print(f"Writing addressbooks for {chain}")
         flatbook = AddrBook(chain).generate_flatbook()
         with open(f"outputs/{chain}.json", "w") as f:
-            json.dump(flatbook, f, indent=3)
+            json.dump(dict(sorted(flatbook.items())), f, indent=3)
         with open(f"outputs/{chain}_reverse.json", "w") as f:
             json.dump(reverse_dict(flatbook), f, indent=3)
 
