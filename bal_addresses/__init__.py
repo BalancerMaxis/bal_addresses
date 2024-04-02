d0796f1ab274108ac052ea608c9c9353a7448ec3
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-04-02 13:46:54 +0200

    chore: run `pre-commit run --all-files`

--- bal_addresses/__init__.py
+++ bal_addresses/__init__.py
@@ -1,3 +1,9 @@
-from .addresses import AddrBook, GITHUB_DEPLOYMENTS_RAW, GITHUB_DEPLOYMENTS_NICE, GITHUB_RAW_OUTPUTS, GITHUB_RAW_EXTRAS
+from .addresses import (
+    AddrBook,
+    GITHUB_DEPLOYMENTS_RAW,
+    GITHUB_DEPLOYMENTS_NICE,
+    GITHUB_RAW_OUTPUTS,
+    GITHUB_RAW_EXTRAS,
+)
 from .permissions import BalPermissions
 from .errors import MultipleMatchesError, NoResultError
