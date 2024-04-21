392d58d9d5a423f0a95c0da016b3396fc2f05886
Tritium-VLK <Tritium-VLK@users.noreply.github.com>
2024-04-21 14:44:43 +0000

    style: ci lint with `black`

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from bal_addresses.pools_gauges import BalPoolsGauges
 from bal_addresses.subgraph import Subgraph
 from bal_addresses.ecosystem import Aura
 
+
 @pytest.fixture(scope="module", params=list(AddrBook.chains["CHAIN_IDS_BY_NAME"]))
 def chain(request):
     chain = request.param
@@ -19,6 +20,7 @@ def bal_pools_gauges(chain):
 @pytest.fixture(scope="module")
 def subgraph(chain):
     return Subgraph(chain)
+
 
 @pytest.fixture(scope="module")
 def aura(chain):
