f095a1abac1d805b29eb73590df9dcc6de0215c1
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-04-24 15:10:28 +0400

    fix: remove goerli, infura endpoint has been removed

--- generate_current_permissions.py
+++ generate_current_permissions.py
@@ -25,7 +25,6 @@ w3_by_chain = {
     "polygon": Web3(
         Web3.HTTPProvider(f"https://polygon-mainnet.infura.io/v3/{INFURA_KEY}")
     ),
-    "goerli": Web3(Web3.HTTPProvider(f"https://goerli.infura.io/v3/{INFURA_KEY}")),
     "sepolia": Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/{INFURA_KEY}")),
 }
 
