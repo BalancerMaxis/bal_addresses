40187539d0a01fe8ae80fa57197008feee314f26
Tritium-VLK <Tritium-VLK@users.noreply.github.com>
2024-04-25 12:49:32 +0000

    style: ci lint with `black`

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,7 @@ import pytest
 from bal_addresses import AddrBook
 
 
-
 @pytest.fixture(scope="module", params=list(AddrBook.chains["CHAIN_IDS_BY_NAME"]))
 def chain(request):
     chain = request.param
     return chain
-
