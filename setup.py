f4433a0f85154fdf3c684ce6c8148368b501f6e2
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-05-10 14:32:57 +0400

    chore: rely on bal tools v0.0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "web3",
         "gql[requests]",
         "requests",
-        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools@v0.0.2",
+        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools@v0.0.3",
     ],
     keywords=["python", "first package"],
     classifiers=[
