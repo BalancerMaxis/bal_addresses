571fd3d66fffdf5dab19ca7e5a1d64a4e87e1fe1
Tritium-VLK <Tritium-VLK@users.noreply.github.com>
2024-07-19 18:52:21 +0000

    style: ci lint with `black`

--- generate_current_permissions.py
+++ generate_current_permissions.py
@@ -18,7 +18,11 @@ DRPC_NAME_OVERRIDES = {
 class W3_RPC:
     def __init__(self, chain, DRPC_KEY):
         drpc_chain = DRPC_NAME_OVERRIDES.get(chain, chain)
-        self.w3 = Web3(Web3.HTTPProvider(f"https://lb.drpc.org/ogrpc?network={drpc_chain}&dkey={DRPC_KEY}"))
+        self.w3 = Web3(
+            Web3.HTTPProvider(
+                f"https://lb.drpc.org/ogrpc?network={drpc_chain}&dkey={DRPC_KEY}"
+            )
+        )
 
     def __getattr__(self, name):
         return getattr(self.w3, name)
@@ -44,7 +48,9 @@ class W3_RPC_BY_CHAIN:
 def build_chain_permissions_list(chain_name):
     a = AddrBook(chain_name)
     results = {}
-    action_ids_list = f"{GITHUB_DEPLOYMENTS_RAW}/action-ids/{chain_name}/action-ids.json"
+    action_ids_list = (
+        f"{GITHUB_DEPLOYMENTS_RAW}/action-ids/{chain_name}/action-ids.json"
+    )
     w3 = W3_RPC(chain_name, os.getenv("DRPC_KEY"))
 
     authorizer = w3.eth.contract(
@@ -66,7 +72,9 @@ def build_chain_permissions_list(chain_name):
                 if numMembers > 0:
                     memberAddressList = []
                     for i in range(0, numMembers, 1):
-                        caller = str(authorizer.functions.getRoleMember(action_id, i).call())
+                        caller = str(
+                            authorizer.functions.getRoleMember(action_id, i).call()
+                        )
                         memberAddressList.append(caller)
 
                     results[action_id] = memberAddressList
