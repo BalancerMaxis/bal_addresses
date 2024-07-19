d6e4e98f1ff7a69eb8cd88ab010bf73a99224806
Tritium-VLK <11885129+Tritium-VLK@users.noreply.github.com>
2024-07-19 20:48:35 +0200

    Add class for web3 connectivity, using dRPC, but easy to change.

--- generate_current_permissions.py
+++ generate_current_permissions.py
@@ -6,47 +6,47 @@ from web3 import Web3
 
 INFURA_KEY = os.getenv("INFURA_KEY")
 ALCHEMY_KEY = os.getenv("ALCHEMY_KEY")
+DRPC_KEY = os.getenv("DRPC_KEY")
 
-w3_by_chain = {
-    "gnosis": Web3(Web3.HTTPProvider(f"https://rpc.gnosischain.com")),
-    "zkevm": Web3(
-        Web3.HTTPProvider(
-            f"https://polygonzkevm-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}"
-        )
-    ),
-    "avalanche": Web3(Web3.HTTPProvider(f"https://api.avax.network/ext/bc/C/rpc")),
-    ### Less reliable RPCs first to fail fast :)
-    #    "linea": Web3(Web3.HTTPProvider(f"https://linea-mainnet.infura.io/v3/{INFURA_KEY}")),
-    "mainnet": Web3(
-        Web3.HTTPProvider(f"https://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
-    "base": Web3(
-        Web3.HTTPProvider(f"https://base-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
-    "arbitrum": Web3(
-        Web3.HTTPProvider(f"https://arb-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
-    "optimism": Web3(
-        Web3.HTTPProvider(f"https://opt-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
-    "polygon": Web3(
-        Web3.HTTPProvider(f"https://polygon-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
-    "mode": Web3(Web3.HTTPProvider("https://mainnet.mode.network/")),
-    "fraxtal": Web3(Web3.HTTPProvider("https://rpc.frax.com")),
-    "sepolia": Web3(
-        Web3.HTTPProvider(f"https://eth-sepolia.g.alchemy.com/v2/{ALCHEMY_KEY}")
-    ),
+
+DRPC_NAME_OVERRIDES = {
+    "mainnet": "ethereum",
+    "zkevm": "polygon-zkevm",
 }
+
+
+class W3_RPC:
+    def __init__(self, chain, DRPC_KEY):
+        drpc_chain = DRPC_NAME_OVERRIDES.get(chain, chain)
+        self.w3 = Web3(Web3.HTTPProvider(f"https://lb.drpc.org/ogrpc?network={drpc_chain}&dkey={DRPC_KEY}"))
+
+    def __getattr__(self, name):
+        return getattr(self.w3, name)
+
+
+class W3_RPC_BY_CHAIN:
+    def __init__(self, DRPC_KEY):
+        self.DRPC_KEY = DRPC_KEY
+        self.w3_by_chain = {}
+        for chain in AddrBook.chain_ids_by_name.keys():
+            self.w3_by_chain[chain] = W3_RPC(chain, DRPC_KEY)
+
+    def __getitem__(self, chain):
+        return self.w3_by_chain[chain]
+
+    def __setitem__(self, chain, value):
+        self.w3_by_chain[chain] = value
+
+    def __delitem__(self, chain):
+        del self.w3_by_chain[chain]
 
 
 def build_chain_permissions_list(chain_name):
     a = AddrBook(chain_name)
     results = {}
-    action_ids_list = (
-        f"{GITHUB_DEPLOYMENTS_RAW}/action-ids/{chain_name}/action-ids.json"
-    )
-    w3 = w3_by_chain[chain_name]
+    action_ids_list = f"{GITHUB_DEPLOYMENTS_RAW}/action-ids/{chain_name}/action-ids.json"
+    w3 = W3_RPC(chain_name, os.getenv("DRPC_KEY"))
+
     authorizer = w3.eth.contract(
         address=a.search_unique("20210418-authorizer/Authorizer").address,
         abi=json.load(open("bal_addresses/abis/Authorizer.json")),
@@ -66,9 +66,7 @@ def build_chain_permissions_list(chain_name):
                 if numMembers > 0:
                     memberAddressList = []
                     for i in range(0, numMembers, 1):
-                        caller = str(
-                            authorizer.functions.getRoleMember(action_id, i).call()
-                        )
+                        caller = str(authorizer.functions.getRoleMember(action_id, i).call())
                         memberAddressList.append(caller)
 
                     results[action_id] = memberAddressList
@@ -82,7 +80,7 @@ def generate_chain_files(chain):
 
 
 def main():
-    for chain in w3_by_chain:
+    for chain in AddrBook.chain_ids_by_name.keys():
         print(f"\n\n\nGenerating Permissions Data for {chain.capitalize()}\n\n\n")
         generate_chain_files(chain)
 
