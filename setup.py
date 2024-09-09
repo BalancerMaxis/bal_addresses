4d60a00d32a4f99ef1391b5e53b61220ee34b8e8
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-09-09 13:04:12 +0200

    chore: bump package version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.9.9"
+VERSION = "0.9.10"
 DESCRIPTION = "Balancer Maxi Addressbook"
 LONG_DESCRIPTION = "Balancer Maxi Addressbook and Balancer Permissions helper"
 
