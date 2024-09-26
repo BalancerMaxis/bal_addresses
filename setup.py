dcd63965f9916a2e0f0c4fcd5f534ea6161e4884
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-09-26 15:55:20 +0200

    chore: bump to latest bal tools release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "setuptools>=42",
         "wheel",
         "pathlib>=1.0",
-        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools.git@v0.1.9",
+        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools.git@v0.1.10",
         "requests",
         "pandas",
         "web3",
