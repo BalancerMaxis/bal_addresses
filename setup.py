65b3b6b3fb6d990146d8f1c1d28514cdfd5734a8
Gosuto Inzasheru <gosuto@inzasheru.email>
2024-12-09 13:30:37 +0700

    chore: bump black to v24.3.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "setuptools>=42",
         "wheel",
         "pathlib>=1.0",
-        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools.git@v0.1.12",
+        "bal_tools @ git+https://github.com/BalancerMaxis/bal_tools.git@ci/dependabot-add-pip",
         "requests",
         "pandas",
         "web3",
